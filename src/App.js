8b076bb62595734a764fe29a5094991e1cd63b00
kiarakim <kiarakim413@gmail.com>
2025-02-10 01:37:11 +0100

    apply material ui

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Customer from './components/Customer';
+import { Paper }   from '@mui/material';
+import { Table } from '@mui/material';
+import { TableHead } from '@mui/material';
+import { TableBody } from '@mui/material';
+import { TableRow } from '@mui/material';
+import { TableCell } from '@mui/material';
+import { styled } from '@mui/material';
 
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  },
+}));
+
+const StyledTable = styled(Table)(({ theme }) => ({
+  table: {
+    minWidth: 1080,
+  },
+}));
 const customers = [
   {
     'id': 1,
-    'image': 'https://picsum.photos/id/236/100/150',
+    'image': 'https://picsum.photos/id/236/40/60',
     'name': 'Kim',
     'birthday': '980413',
     'gender': 'female',
@@ -13,7 +33,7 @@ const customers = [
   },
   {
     'id': 2,
-    'image': 'https://picsum.photos/id/237/100/150',
+    'image': 'https://picsum.photos/id/237/40/60',
     'name': 'Gwon',
     'birthday': '991213',
     'gender': 'male',
@@ -21,7 +41,7 @@ const customers = [
   },
   {
     'id': 3,
-    'image': 'https://picsum.photos/id/238/100/150',
+    'image': 'https://picsum.photos/id/238/40/60',
     'name': 'Yoo',
     'birthday': '666666',
     'gender': 'female',
@@ -29,26 +49,28 @@ const customers = [
   }
 ]
 
-function App() {
-  return (
-    <div>
-      {
-        customers.map(c => {
-          return (
-            <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name}
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-            />
-          )
-        })
-      }
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <StyledPaper>
+        <StyledTable>
+          <TableHead>
+            <TableRow>
+              <TableCell>no.</TableCell>
+              <TableCell>image</TableCell>
+              <TableCell>name</TableCell>
+              <TableCell>birthday</TableCell>
+              <TableCell>gender</TableCell>
+              <TableCell>job</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>)})}
+          </TableBody>
+        </StyledTable>
+      </StyledPaper>
+    );
+  }
 }
 
 export default App;
